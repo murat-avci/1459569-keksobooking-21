7fae0db0612f676dd61e10e689220f9a3fd6164b
murat-avci <avcmurat@yahoo.com>
2020-10-01 10:41:37 +0300

    Личный проект: доверяй, но проверяй (часть 1)

--- js/main.js
+++ js/main.js
@@ -22,6 +22,8 @@ const MIN_PRICE = 1000;
 const MAX_PRICE = 1000000;
 const MIN_ROOM = 1;
 const MAX_ROOM = 5;
+const MIN_GUESTS = 1;
+const MAX_GUESTS = 5;
 const MIN_LOCATION_X = 100;
 const MAX_LOCATION_X = 900;
 const MIN_LOCATION_Y = 130;
@@ -29,6 +31,13 @@ const MAX_LOCATION_Y = 630;
 const IMAGE_NUM_RANGES = [1, 2, 3, 4, 5, 6, 7, 8];
 const IMG_WIDTH = 45;
 const IMG_HEIGHT = 40;
+const PIN_WIDTH = 65;
+const PIN_HEIGHT = 65;
+const PIN_OFFSET_X = 570;
+const PIN_OFFSET_Y = 375;
+const POINTER_HEIGHT = 22;
+
+const ESC_KEYCODE = 27;
 
 const TypeOfHouses = {
   'flat': `Квартира`,
@@ -38,17 +47,26 @@ const TypeOfHouses = {
 };
 
 
-const map = document.querySelector(`.map`);
-map.classList.remove(`map--faded`);
-
+const mapSection = document.querySelector(`.map`);
+const fieldsets = document.querySelectorAll(`fieldset`);
+const selects = document.querySelectorAll(`select[name^=housing]`);
 const mapPinList = document.querySelector(`.map__pins`);
 const pinTemplate = document.querySelector(`#pin`)
   .content
   .querySelector(`.map__pin`);
-const mapCard = document.querySelector(`.map`);
 const cardTemplate = document.querySelector(`#card`)
   .content
   .querySelector(`.map__card`);
+
+const mainPin = document.querySelector(`.map__pin--main`);
+const advertForm = document.querySelector(`.ad-form`);
+const inputAddress = document.querySelector(`#address`);
+const inputAddressLoad = `${Math.floor(PIN_OFFSET_X + (PIN_WIDTH / 2))} , ${Math.floor(PIN_OFFSET_Y + (PIN_HEIGHT / 2))}`;
+const inputAddressActive = `${Math.floor(PIN_OFFSET_X + (PIN_WIDTH / 2))} , ${Math.floor((PIN_OFFSET_Y + PIN_HEIGHT + POINTER_HEIGHT))}`;
+
+let activeCardId;
+let currentPin = null;
+let currentCard = null;
 
 const getUnique = function (titles) {
   const uniqueEl = titles[getRandom(0, titles.length)];
@@ -60,16 +78,17 @@ const getRandom = function (min, max) {
   return Math.floor(Math.random() * max + min);
 };
 
-const compareRandom = function () {
-  return Math.random() - 0.5;
+const shuffleArray = function (array) {
+  const finalArr = array.slice();
+  for (let i = 0; i < finalArr.length; i++) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let tmp = finalArr[i];
+    finalArr[i] = finalArr[j];
+    finalArr[j] = tmp;
+  }
+  return finalArr;
 };
 
-const getRandomFeatures = function (array) {
-  array.length = getRandom(1, array.length);
-  return array;
-};
-
-const customFeatures = getRandomFeatures(FEATURES);
 const createObject = function () {
 
   const locationX = getRandom(MIN_LOCATION_X, MAX_LOCATION_X);
@@ -83,14 +102,14 @@ const createObject = function () {
       title: getUnique(TITLES),
       address: `${locationX}, ${locationY}`,
       price: getRandom(MIN_PRICE, MAX_PRICE),
-      type: OFFER_TYPES[getRandom(0, OFFER_TYPES.length)],
-      rooms: getRandom(MIN_ROOM, MAX_ROOM),
-      guests: getRandom(1, 5),
+      type: OFFER_TYPES[getRandom(0, OFFER_TYPES.length + 1)],
+      rooms: getRandom(MIN_ROOM, MAX_ROOM + MIN_ROOM),
+      guests: getRandom(MIN_GUESTS, MAX_GUESTS + 1),
       checkin: CHECK_TIMES[getRandom(0, CHECK_TIMES.length)],
       checkout: CHECK_TIMES[getRandom(0, CHECK_TIMES.length)],
-      features: customFeatures,
+      features: shuffleArray(FEATURES.slice(0, getRandom(0, FEATURES.length + 1))),
       description: DESCRIPTIONS,
-      photos: PHOTOS.sort(compareRandom),
+      photos: shuffleArray(PHOTOS),
     },
     location: {
       x: locationX,
@@ -145,7 +164,7 @@ const createPins = function (icons) {
   }
 };
 
-const createCard = function (item) {
+const getCardData = function (item) {
   const cardItem = cardTemplate.cloneNode(true);
   const roomNum = item.offer.rooms;
   const guestNum = item.offer.guests;
@@ -154,14 +173,13 @@ const createCard = function (item) {
   const roomPhrase = roomNum === 1 ? ` комната для ` : `комнаты для`;
 
 
-  mapCard.insertBefore(cardItem, mapPinList);
+  mapSection.insertBefore(cardItem, mapPinList);
   cardItem.querySelector(`.popup__title`).textContent = item.offer.title;
   cardItem.querySelector(`.popup__text--address`).textContent = item.offer.address;
   cardItem.querySelector(`.popup__text--price`).innerHTML = `${item.offer.price} &#x20bd/ночь`;
   cardItem.querySelector(`.popup__type`).textContent = TypeOfHouses[item.offer.type];
   cardItem.querySelector(`.popup__text--capacity`).textContent = `${roomNum}${roomPhrase} для ${guestNum}${guestPhrase}`;
   cardItem.querySelector(`.popup__text--time`).textContent = `Заезд после ${item.offer.checkin} выезд после ${item.offer.checkout}`;
-  const fragmentCard = document.createDocumentFragment();
   const cardFeatures = cardItem.querySelector(`.popup__features`);
   cardFeatures.innerHTML = ``;
   cardFeatures.appendChild(createFragmentFeatures(item.offer.features));
@@ -170,13 +188,93 @@ const createCard = function (item) {
   cardPhotos.innerHTML = ``;
   cardPhotos.appendChild(createFragmentPhotos(item.offer.photos));
   cardItem.querySelector(`.popup__avatar`).src = item.author.avatar;
-
-  fragmentCard.appendChild(cardItem);
-  mapCard.appendChild(fragmentCard);
+  mapSection.appendChild(cardItem);
 };
 
 const cards = createData();
 
-createPins(cards);
-createCard(cards[0]);
+const toggleDisabled = function (isDisabled, nodes) {
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].disabled = isDisabled;
+  }
+};
 
+window.addEventListener(`load`, function () {
+  toggleDisabled(true, fieldsets);
+  toggleDisabled(true, selects);
+  inputAddress.value = inputAddressLoad;
+});
+
+const onButtonMouseDown = function () {
+  inputAddress.disabled = true;
+  inputAddress.value = inputAddressActive;
+  mapSection.classList.remove(`map--faded`);
+  advertForm.classList.remove(`ad-form--disabled`);
+  createPins(cards);
+  toggleDisabled(false, fieldsets);
+  toggleDisabled(false, selects);
+  mainPin.addEventListener(`mousedown`, removeOnButtonMouseDown);
+};
+
+const createCard = function (id) {
+  activeCardId = id;
+  currentCard = mapSection.appendChild(getCardData(cards[id]));
+  document.addEventListener(`keydown`, onPopupEscPress);
+};
+
+const removeCard = function () {
+  if (currentCard) {
+    mapSection.removeChild(currentCard);
+    currentCard = null;
+  }
+};
+
+const onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    removeCard();
+    document.removeEventListener(`keydown`, onPopupEscPress);
+    currentPin.classList.remove(`map__pin--active`);
+    activeCardId = null;
+    currentPin.blur();
+  }
+};
+
+const checkActive = function () {
+  removeCard();
+  if (currentPin) {
+    currentPin.classList.remove(`map__pin--active`);
+    activeCardId = null;
+  }
+};
+
+const showCard = function (evt) {
+  const target = evt.target;
+  const pinButton = target.closest(`.map__pin:not(.map__pin--main)`);
+  const buttonClose = target.className === `popup__close`;
+
+  if (buttonClose) {
+    checkActive();
+  }
+
+  if (!pinButton || (pinButton && activeCardId === pinButton.dataset.id)) {
+    return;
+  }
+
+  checkActive();
+
+  currentPin = pinButton;
+
+  createCard(pinButton.dataset.id);
+
+  pinButton.classList.add(`map__pin--active`);
+};
+
+mapSection.addEventListener(`click`, function (evt) {
+  showCard(evt);
+});
+
+const removeOnButtonMouseDown = function () {
+  mainPin.removeEventListener(`mousedown`, onButtonMouseDown);
+};
+
+mainPin.addEventListener(`mousedown`, onButtonMouseDown);
