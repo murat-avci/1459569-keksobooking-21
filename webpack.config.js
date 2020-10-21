38d10d2c6bd70797af1f330929d964ceaae78f3d
murat-avci <avcmurat@yahoo.com>
2020-10-21 16:54:35 +0300

    Revert "Update webpack.config.js"
    This reverts commit c695a350ca391a7eb271e18bda46ced47bdda0b2.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require(`path`);
+const path = require('path');
 
 module.exports = {
   entry: [
-    `./js/backend.js`,
-    `./js/debounce.js`,
-    `./js/constants.js`,
-    `./js/elements.js`,
-    `./js/filter.js`,
-    `./js/form.js`,
-    `./js/main.js`,
-    `./js/pin.js`,
-    `./js/map.js`,
-    `./js/card.js`,
-    `./js/show-cards.js`,
+    './js/backend.js',
+    './js/debounce.js',
+    './js/constants.js',
+    './js/elements.js',
+    './js/filter.js',
+    './js/form.js',
+    './js/main.js',
+    './js/pin.js',
+    './js/map.js',
+    './js/card.js',
+    './js/show-cards.js',
   ],
   output: {
-    filename: `bundle.js`,
+    filename: 'bundle.js',
     path: path.resolve(__dirname),
     iife: true
   },
