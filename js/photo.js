cc44a1e775350d886ef7bd53fd544a03f4393b21
murat-avci <avcmurat@yahoo.com>
2020-10-23 09:18:18 +0300

    Личный проект: фото на память

--- js/photo.js
+++ js/photo.js
@@ -2,65 +2,61 @@
 
 const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
 
-const avatarChooser = window.elements.avatarContainer.querySelector(`input[type=file]`);
-const photoChooser = window.elements.photoContainer.querySelector(`input[type=file]`);
+const avatarChooser = document.querySelector(`#avatar`);
+const avatarPreview = document.querySelector(`.ad-form-header__preview img`);
+const photoChooser = document.querySelector(`#images`);
 const photoForm = document.querySelector(`.ad-form__photo`);
+
+const matchTypes = function (file) {
+  return FILE_TYPES.some(function (it) {
+    return file.name.toLowerCase().endsWith(it);
+  });
+};
+
+const changeAvatar = function (src) {
+  avatarPreview.src = src;
+};
 
 const createImg = function (tagName, resultReader) {
   const imgElement = document.createElement(tagName);
   imgElement.src = resultReader;
-  imgElement.style.width = window.constants.PHOTO_WIDTH;
-  imgElement.style.height = window.constants.PHOTO_HEIGHT;
+  imgElement.style.width = `${window.constants.PHOTO_WIDTH}px`;
+  imgElement.style.height = `${window.constants.PHOTO_HEIGHT}px`;
   return imgElement;
 };
 
-const matchTypes = function (name) {
-  return FILE_TYPES.some(function (it) {
-    return name.endsWith(it);
-  });
-};
-
-const onAvatarLoad = function (evt) {
-  const fileChooser = evt.target;
-  const file = fileChooser.files[0];
-  const fileName = file.name.toLowerCase();
-
-  if (matchTypes(fileName)) {
-    const reader = new FileReader();
-
-    reader.addEventListener(`load`, function () {
-      const result = reader.result;
-      window.elements.previewContainer.src = result;
-    });
-
-    reader.readAsDataURL(file);
-
+const addPhoto = function (result) {
+  if (!photoForm.hasChildNodes()) {
+    photoForm.appendChild(createImg(`img`, result));
+  } else {
+    const divElement = document.createElement(`div`);
+    divElement.classList.add(`ad-form__photo`);
+    divElement.appendChild(createImg(`img`, result));
+    window.elements.photoContainer.appendChild(divElement);
   }
 };
 
-const onPhotoLoad = function (evt) {
-  const fileChooser = evt.target;
-  const file = fileChooser.files[0];
-  const fileName = file.name.toLowerCase();
-
-  if (matchTypes(fileName)) {
-    const reader = new FileReader();
-
-    reader.addEventListener(`load`, function () {
-      const result = reader.result;
-
-      if (!photoForm.hasChildNodes()) {
-        photoForm.appendChild(createImg(`img`, result));
-      } else {
-        const divElement = document.createElement(`div`);
-        divElement.classList.add(`ad-form__photo`);
-        divElement.appendChild(createImg(`img`, result));
-        window.elements.photoContainer.appendChild(divElement);
-      }
+const loadFile = function (chooser, func) {
+  const files = Array.from(chooser.files).filter(matchTypes);
+  if (files) {
+    files.forEach(function (it) {
+      const reader = new FileReader();
+      reader.addEventListener(`load`, function (evt) {
+        func(evt.target.result);
+      });
+      reader.readAsDataURL(it);
     });
-    reader.readAsDataURL(file);
   }
 };
 
-avatarChooser.addEventListener(`change`, onAvatarLoad);
-photoChooser.addEventListener(`change`, onPhotoLoad);
+const onAvatarChange = function (evt) {
+  loadFile(evt.target, changeAvatar);
+};
+
+const onPhotoChange = function (evt) {
+  loadFile(evt.target, addPhoto);
+};
+
+avatarChooser.addEventListener(`change`, onAvatarChange);
+photoChooser.addEventListener(`change`, onPhotoChange);
+
